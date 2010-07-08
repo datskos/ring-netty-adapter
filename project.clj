d1c25df5833b2bc28788c3d5fc09b34107433fe0
datskos <datskosgeorge@gmail.com>
2010-07-08 19:54:52 +0900

    bump Ring dependency to 0.2.5

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ring-netty-adapter "0.0.2"
+(defproject ring-netty-adapter "0.0.3"
   :repositories [["JBoss" "http://repository.jboss.org/nexus/content/groups/public/"]]
   :description "Ring Netty adapter"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [org.jboss.netty/netty       "3.2.1.Final"]
-                 [ring "0.2.4"]]
+                 [ring "0.2.5"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
   :namespaces [ring.adapter.netty ring.adapter.plumbing])
