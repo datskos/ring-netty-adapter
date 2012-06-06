fc3d705167a628946d413ee86f25ea8066530d9e
Adam Esterline <adam@esterlines.com>
2012-06-06 08:00:42 -0600

    Added more integration tests to cover other types of responses

--- test/clojure/ring/adapter/test/integration_examples.clj
+++ test/clojure/ring/adapter/test/integration_examples.clj
@@ -2,20 +2,18 @@
   (:use expectations
         ring.util.response)
   (:require [ring.adapter.netty :as netty]
-              [clj-http.client :as client]
-              [cheshire.core :as json]
-              [clojure.java.io :as io]))
+            [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.java.io :as io]))
 
-;; zerocopy
-;;
+(declare request-to response-handler file-handler header-handler stream-handler seq-handler body-handler server)
 
-(declare request response-handler file-handler header-handler stream-handler seq-handler body-handler)
-
-(expect (request {:headers {"Sample" "fo"}} header-handler) "fo")
-(expect (request {} file-handler) "Help")
-(expect (request {} stream-handler) "Help")
-(expect (request {} seq-handler) "Help")
-(expect (request {:body "Help"} body-handler) "Help")
+(expect "fo" (request-to (server header-handler) {:headers {"Sample" "fo"}}))
+(expect "Help" (request-to (server file-handler)))
+(expect "Help" (request-to (server stream-handler)))
+(expect "Helper" (request-to (server seq-handler)))
+(expect "Help Me" (request-to (server body-handler) {:body "Help Me"}))
+(expect "Help" (request-to (server file-handler {:zerocopy true})))
 
 (expect {:uri "/part"
          :query-string "key=value"
@@ -26,15 +24,21 @@
          :request-method "post"
          :remote-addr "127.0.0.1"
          :content-type "application/json"
-         :content-length 0} (in (request {:content-type :json :as :json} response-handler)))
+         :content-length 0} (in (request-to (server response-handler) {:content-type :json :as :json})))
 
-(defn request [options handler]
-  (with-open [server (netty/run-netty handler {:port 8081})]
-    (:body (client/post "http://localhost:8081/part?key=value" options))))
+(defn request-to
+  ([netty] (request-to netty {}))
+  ([netty request-options]
+    (with-open [server netty]
+      (:body (client/post "http://localhost:8081/part?key=value" request-options)))))
 
-(defn response-handler [req] (response (json/generate-string (dissoc req :request :context :body))))
+(defn response-handler [req] (response (json/generate-string (dissoc req :request :context :body ))))
 (defn file-handler [req] (response (io/file "test/clojure/ring/adapter/test/sample.txt")))
 (defn stream-handler [req] (response (io/input-stream "test/clojure/ring/adapter/test/sample.txt")))
-(defn seq-handler [req] (response '("H" "e" "l" "p")))
+(defn seq-handler [req] (response '("H" "e" "l" "p" "e" "r")))
 (defn header-handler [req] (response (get-in req [:headers "sample"])))
 (defn body-handler [req] (response (slurp (:body req))))
+
+(defn server
+  ([handler] (server handler {}))
+  ([handler options] (netty/run-netty handler (merge {:port 8081} options))))
