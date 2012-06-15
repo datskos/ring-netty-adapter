6a56cf3582fbc28e11bb36e0eae42ac7fb2955aa
Adam Esterline <adam@esterlines.com>
2012-06-14 21:46:01 -0600

    Upgraded to the latest version of netty

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-netty-adapter/ring-netty-adapter "0.0.3"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.jboss.netty/netty "3.2.7.Final"]
+                 [io.netty/netty "3.5.0.Final"]
                  [ring "1.1.0"]
                  [clj-http "0.4.2"]
                  [cheshire "4.0.0"]]
@@ -8,7 +8,5 @@
   :test-paths ["test/clojure"]
   :profiles {:dev {:dependencies [[expectations "1.4.3"]
                                   [junit/junit "4.8.1"]]}}
-  :repositories {"JBoss"
-                 "http://repository.jboss.org/nexus/content/groups/public/"}
   :min-lein-version "2.0.0"
   :description "Ring Netty adapter")
